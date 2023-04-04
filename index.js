dc471f7689a7a92caee3367cd0df89fccf1b3a02
oi <105376328+delfo2@users.noreply.github.com>
2023-04-04 14:52:17 -0300

    basico do projeto

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('olá mundo');
+import chalk from 'chalk';
+import fs from 'fs';
+
+function pegaArquivo (caminhoArquivo) {
+    fs.readFile(caminhoArquivo, 'utf-8', (_, res) => {
+        console.log(chalk.inverse(res));
+    })
+}
+
+pegaArquivo('./arquivos/texto.md');
